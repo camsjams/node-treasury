9885dab754cf8fdc57648d72f17ad9a8351aac7f
Cameron Manavian <cameron.manavian@gmail.com>
2015-12-21 23:19:16 -0800

    flow

--- index.js
+++ index.js
@@ -30,6 +30,16 @@ function Treasury(opts) {
     };
 
     function invest(thePromise, options) {
+        // get from cache
+            // if found in cache
+                // return value via promise
+            // if not found in cache
+                // run promise for value
+                    // if promise successful
+                        // set to cache
+                            // return data via promise
+        // end consumer handles all catches!
+
         return config.promiseFactory(function(resolve, reject) {
             thePromise
                 .then(function() {
