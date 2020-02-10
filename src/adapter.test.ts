a994121e5fad0370ceacc4d92342e50126043195
Cameron Manavian <cameron.manavian@gmail.com>
2020-02-10 12:24:28 -0800

    Add coverage

--- src/adapter.test.ts
+++ src/adapter.test.ts
@@ -19,6 +19,7 @@ test('should throw error for undefined as invalid_client', () => {
 	chai.assert.typeOf(error, 'Error');
 	chai.assert.equal(error.message, 'invalid_client');
 });
+
 test('should throw error for falsy as invalid_client', () => {
 	// arrange
 	let result;
@@ -36,6 +37,7 @@ test('should throw error for falsy as invalid_client', () => {
 	chai.assert.typeOf(error, 'Error');
 	chai.assert.equal(error.message, 'invalid_client');
 });
+
 test('should return default client adapter', () => {
 	// arrange
 	// act
@@ -48,6 +50,45 @@ test('should return default client adapter', () => {
 	chai.assert.typeOf(result.del, 'Function');
 	chai.assert.typeOf(result.client, 'Null');
 });
+
+test('should return Redis client adapter', () => {
+	// arrange
+	const EXPECTED = {
+		constructor: {
+			name: 'RedisClient'
+		}
+	};
+
+	// act
+	const result = getAdapter(EXPECTED);
+
+	// assert
+	chai.assert.typeOf(result, 'Object');
+	chai.assert.typeOf(result.get, 'Function');
+	chai.assert.typeOf(result.set, 'Function');
+	chai.assert.typeOf(result.del, 'Function');
+	expect(result.client).toEqual(EXPECTED);
+});
+
+test('should return Memcached client adapter', () => {
+	// arrange
+	const EXPECTED = {
+		constructor: {
+			name: 'Client'
+		}
+	};
+
+	// act
+	const result = getAdapter(EXPECTED);
+
+	// assert
+	chai.assert.typeOf(result, 'Object');
+	chai.assert.typeOf(result.get, 'Function');
+	chai.assert.typeOf(result.set, 'Function');
+	chai.assert.typeOf(result.del, 'Function');
+	expect(result.client).toEqual(EXPECTED);
+});
+
 test('should throw error for unknown as unknown_client', () => {
 	// arrange
 	let result;
